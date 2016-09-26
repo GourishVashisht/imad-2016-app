bd889eb9a2e3d338d538d61ab5dc42ba0f25590e
Gourish Vashisht <gourish.gv@gmail.com>
2016-09-26 21:58:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,15 +17,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/article-one', function(req, res){
-   res.send( 'this is the first article and i have edited it myself.');
-});
-
-app.get('/article-two', function(req, res){
-   res.send( 'this is the second article and i have edited it myself.');
-});
-
-
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
